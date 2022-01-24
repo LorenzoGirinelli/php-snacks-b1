deb2e805e03ea79b5a8977da237a1de9b66c581d
LorenzoGirinelli <girinellilorenzo22@hotmail.com>
2022-01-24 17:35:30 +0100

    Snack 4

---
+++ snack-4/index.php
@@ -0,0 +1,5 @@
+<?php 
+    $text = ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis accusamus enim assumenda earum fugit iste. odio nulla officiis iure dolore, ad excepturi in perferendis. amet vel officia dolorum debitis nisi?';
+    $spliceText = explode(".", $text);
+    var_dump($spliceText);
+?>
